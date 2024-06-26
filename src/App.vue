46b1ddf71365867e0694e7a64162f4de4eb7330d
Benjamin Cecile <bencecile@gmail.com>
2024-06-26 10:53:57 -0400

    Basic Vue app with Counter state

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { State } from '@/state/state';
+</script>
+
+<template>
+  <div class="wrapper">
+    <div>{{ State.count }} ({{ State.doubleCount }})</div>
+    <button @click="State.increment">Increment</button>
+  </div>
+</template>
+
+<style scoped></style>
