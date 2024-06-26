18c79e973dd1856dc7db5de25f039456efead5f9
Benjamin Cecile <bencecile@gmail.com>
2024-06-26 11:12:34 -0400

    Changes for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CultivationSamsara/",
   plugins: [
     vue(),
   ],
@@ -12,8 +13,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    outDir: "docs"
   }
 })
